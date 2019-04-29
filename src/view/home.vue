bb3afa5a0a14331fe33a8adb7e3aa3fae85657d2
luomen1126 <1689488576@qq.com>
2019-04-29 18:26:53 +0800

    first

---
+++ src/view/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    首页
+    <button @click="btn">获取数据</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  methods: {
+    btn () {
+      this.$axios.post('/local/app/home.do').then(function (response) {
+        console.log(response)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
